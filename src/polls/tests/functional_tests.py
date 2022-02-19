6fa50df751e357b82b686d15b16891210e506430
ilya <ikos.deve@gmail.com>
2022-02-19 11:25:34 +0300

    fix functional_tests.py

--- src/polls/tests/functional_tests.py
+++ src/polls/tests/functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
 
 browser = webdriver.Firefox()
-browser.get('http://0.0.0.0:8010')
+browser.get('http://0.0.0.0:8000')
+print(browser.title)
+assert 'Django' in browser.title
